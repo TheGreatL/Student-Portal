59ac5bda46ea09de6e2bc45823035782b23d1ce8
Ryuzaki <bpow2308@gmail.com>
2025-03-12 20:49:21 +0800

    Remove prisma comments

--- app/(student)/page.tsx
+++ app/(student)/page.tsx
@@ -1,35 +1,11 @@
 import HomeCarousel from '@/components/home-carousel';
 import CoursesTabs from '@/components/home/courses-tabs';
-import Users from '@/components/users';
-// import {prisma} from '@/utils';
-import {Suspense} from 'react';
+
 export default async function HomePage() {
-  // const createUser = await prisma.user.create({
-  //   data: {
-  //     firstName: 'Ken',
-  //     lastName: 'Carlon',
-  //     email: 'carlon.309926@fairview.sti.edu.ph',
-  //     password: 'asd',
-  //     avatar: 'avatarss',
-  //     role: 'student',
-  //     teacher: {
-  //       create: {
-  //         department: 'ict'
-  //       }
-  //     }
-  //   }
-  // });
-
-  // console.log(createUser);
-
   return (
     <section className='flex grow flex-col gap-5 px-1 py-2'>
       <HomeCarousel />
       <CoursesTabs />
-      {/* waits async server components */}
-      <Suspense fallback={<p>Loading.....</p>}>
-        <Users />
-      </Suspense>
     </section>
   );
 }
--- components/users.tsx
+++ components/users.tsx
@@ -1,53 +1,7 @@
-import {TStudent, TTeacher} from '@/types/types';
-import {prisma} from '@/utils';
-
 export default async function Users() {
-  const allStudents = await prisma.student.findMany({
-    include: {
-      user: true
-    }
-  });
-
-  const allTeacher = await prisma.teacher.findMany({
-    include: {
-      user: true
-    }
-  });
-
   return (
     <div className='flex flex-col'>
-      <div className='flex flex-wrap items-center justify-center gap-2'>
-        {allStudents.map((student: TStudent) => (
-          <div
-            key={student.id}
-            className='flex w-[10rem] grow flex-col bg-gray-400 text-wrap break-words'>
-            <p>{student.id}</p>
-            <p>{student.user.firstName}</p>
-            <p>{student.user.lastName}</p>
-            <p>{student.userId}</p>
-            <p>{student.user.createdAt?.toDateString()}</p>
-            <p>{student.user.updatedAt?.toDateString()}</p>
-            <p>Deleted At: {student.user.deletedAt?.toDateString()}</p>
-            <p>Course: {student.course}</p>
-          </div>
-        ))}
-      </div>
-      <div className='flex flex-wrap items-center justify-center gap-2'>
-        <h1>Teachers</h1>
-        {allTeacher.map((teacher: TTeacher) => (
-          <div
-            key={teacher.id}
-            className='flex w-[10rem] grow flex-col bg-gray-400 text-wrap break-words'>
-            <p>{teacher.id}</p>
-            <p>{teacher.user.firstName}</p>
-            <p>{teacher.user.lastName}</p>
-            <p>{teacher.userId}</p>
-            <p>{teacher.user.createdAt?.toDateString()}</p>
-            <p>{teacher.user.updatedAt?.toDateString()}</p>
-            <p>Deleted At: {teacher.user.deletedAt?.toDateString()}</p>
-          </div>
-        ))}
-      </div>
+      <h1>test</h1>
     </div>
   );
 }

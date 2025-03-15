e58d69df8938f70e887c6d978e226552d0129983
Ryuzaki <bpow2308@gmail.com>
2025-03-15 23:59:34 +0800

    fixed: pagination navigator

--- components/users-student-table-pagination.tsx
+++ components/users-student-table-pagination.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationNext,
-  PaginationPrevious
-} from '@/components/ui/pagination';
+import {Pagination, PaginationContent, PaginationItem} from '@/components/ui/pagination';
+import Link from 'next/link';
+import {ChevronLeftIcon, ChevronRightIcon} from 'lucide-react';
 export default function UsersStudentTablePagination({page, isMax}: {page: number; isMax: boolean}) {
   console.log(isMax);
   return (
     <Pagination className='bg-white p-2'>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious
+          <Link
             aria-disabled={page === 1}
-            className={`${page === 1 && 'pointer-events-none opacity-50'}`}
-            href={`/admin/users?page=${page > 1 ? page - 1 : 1}`}
-          />
+            className={`${page === 1 && 'f; pointer-events-none opacity-50'} flex rounded-2xl border-2 border-black px-2 hover:bg-gray-300`}
+            href={`/admin/users?page=${page > 1 ? page - 1 : 1}`}>
+            <>
+              <ChevronLeftIcon />
+              <span className='hidden sm:block'>Previous</span>
+            </>
+          </Link>
         </PaginationItem>
         <PaginationItem>
-          <PaginationNext
+          <Link
             aria-disabled={isMax}
-            className={`${isMax && 'pointer-events-none opacity-50'}`}
-            href={`/admin/users?page=${page + 1}`}
-          />
+            className={`${isMax && 'pointer-events-none opacity-50'} flex rounded-2xl border-2 border-black px-2 hover:bg-gray-300`}
+            href={`/admin/users?page=${page + 1}`}>
+            <span className='hidden sm:block'>Next</span>
+            <ChevronRightIcon />
+          </Link>
         </PaginationItem>
       </PaginationContent>
     </Pagination>

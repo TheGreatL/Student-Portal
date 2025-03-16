4473aa5f3fa1201e83d4525c4c8fdd0d26ee73c4
Ryuzaki <bpow2308@gmail.com>
2025-03-16 12:45:45 +0800

    fixed: users props

--- features/users/components/users.tsx
+++ features/users/components/users.tsx
@@ -1,13 +1,13 @@
 import UsersStudentTablePagination from './users-student-table-pagination';
 
-export default async function Users({page}: {page: number}) {
+export default async function Users({page, numberOfPage}: {page: number; numberOfPage: number}) {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return (
     <div className='flex flex-col'>
       <h1>test</h1>
       <UsersStudentTablePagination
         page={page}
-        isMax={false}
+        numberOfPage={numberOfPage}
       />
     </div>
   );
